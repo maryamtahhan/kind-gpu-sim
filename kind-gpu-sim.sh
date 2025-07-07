92cb72b46095be1d1095e88ea790404bd0ad75d0
Maryam Tahhan <mtahhan@redhat.com>
2025-07-07 08:27:30 -0400

    chore: check kind cluster exists before deleting it
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- kind-gpu-sim.sh
+++ kind-gpu-sim.sh
@@ -278,7 +278,12 @@ EOF
 }
 
 function delete_cluster() {
-  kind delete cluster --name "${CLUSTER_NAME}"
+  if kind get clusters | grep -qx "${CLUSTER_NAME}"; then
+    echo "Deleting kind cluster '${CLUSTER_NAME}'..."
+    kind delete cluster --name "${CLUSTER_NAME}"
+  else
+    echo "Kind cluster '${CLUSTER_NAME}' does not exist. Skipping delete."
+  fi
 }
 
 function delete_registry() {
